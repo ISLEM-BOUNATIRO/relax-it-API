44cd74b7449cc6b7dd5329c73df0b949dcc634fa
ISLEM-BOUNATIRO <bntr.islem@gmail.com>
2022-09-11 13:49:22 +0100

    Access Firewall

--- test.py
+++ test.py
@@ -48,31 +48,5 @@ def excute_script(ip,username,password,commands):
 
 
 
- 
-#---------------------------------------------------------
-class pp:
-    def is_big():
-        return True
-tn=telnetlib.Telnet("192.168.217.254")
-tn.read_until(b"login: ")
-tn.write("admin".encode('ascii') + b"\n")
-tn.read_until(b"Password: ")
-tn.write("admin".encode('ascii') + b"\n")
-while (pp.is_big()):
-    command=input()
-    command=command+"\n"
-    tn.write(command.encode('ascii'))
-    tn.write("endeocrino\n".encode('ascii'))
-    output=tn.read_until(b"# endeocrino\r\r\nUnknown action 0").decode('ascii')
-    output=output.split('\n')
-    output=output[1:-2]
-    last_output=""
-    for line in output:
-        last_output=last_output+"\n"+line
-    print(last_output)
-
-
-
-
 
 
